ac77b0b444c6ca641e7301ef88a8d7c3c841ceb4
Abdurrazzak Rauff Shaikh <152030209+Razaumar3124@users.noreply.github.com>
2025-07-28 01:45:54 +0530

    all done with application

--- src/AdvanceAnimations/AdvAnim1.jsx
+++ src/AdvanceAnimations/AdvAnim1.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Cocktails from './Components/Cocktails'
+import About from './Components/About'
+import Art from './Components/Art'
+import Menu from './Components/Menu'
+import Contact from './Components/Contact'
 
 const AdvAnim1 = () => {
   return (
     <main>
       <Navbar />
       <Hero />
-      <div className='h-dvh bg-black' />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   )
 }
--- src/AdvanceAnimations/Components/Hero.jsx
+++ src/AdvanceAnimations/Components/Hero.jsx
@@ -1,9 +1,14 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap';
 import { SplitText } from 'gsap/all';
-import React from 'react'
+import React, { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
+
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 }); //width less than 767 will be consider as mobile above that is laptop
 
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
@@ -38,6 +43,26 @@ const Hero = () => {
     })
     .to(".right-leaf", { y: 200 }, 0)
     .to(".left-leaf", { y: -200 }, 0)
+
+    const startValue = isMobile ? "center 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true, //this property will keep the video stick to the screen
+      }
+    })
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration
+      })
+    }
+
   }, []);
 
   return (
@@ -76,6 +101,16 @@ const Hero = () => {
         </div>
       </div>
     </section>
+
+    <div className='video absolute inset-0'>
+      <video 
+      ref={videoRef}
+      src="/videos/output.mp4" 
+      muted 
+      playsInline
+      preload='auto'
+      />
+    </div>
     </>
   )
 }
